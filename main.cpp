ebe8e3ea2e350904baf4fb5e4a025d40c448a48b
nina <juan.amado.sicat.cleto@gmail.com>
2025-06-01 19:51:08 +0800

    BRO, there is python and js here haha

--- main.cpp
+++ main.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
+const int MAX_SIZE = 5;
+typedef int ELEMENT_TYPE;
+
+bool isFull(int last) {
+    if (last == MAX_SIZE - 1) {
+        return true;
+    }
+
+    return false;
+}
+
+bool isEmpty(int last) {
+    if (last == -1) {
+        return true;
+    }
+    return false;
+}
+
+
+
+
 int main() {
-    cout << "Hello World" << endl;
-    cout << "First time trying out CLION" << endl;
+
+
+
 }
